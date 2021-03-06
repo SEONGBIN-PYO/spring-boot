67da345559898a004cd1f014191171470dbac3fa
SEONGBIN-PYO <binch3627@gmail.com>
2021-03-06 19:35:09 +0900

    profile api 추가

--- src/main/java/com/book/springboot/web/HelloController.java
+++ src/main/java/com/book/springboot/web/HelloController.java
@@ -1,12 +1,19 @@
 package com.book.springboot.web;
 
 import com.book.springboot.web.dto.HelloResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class HelloController {
+    private final Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -18,4 +25,20 @@ public class HelloController {
                                      @RequestParam("amount") int amount) {
         return new HelloResponseDto(name, amount);
     }
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+
+        if(profiles.contains("real1")){
+            return "real1";
+        }
+
+        if(profiles.contains("real2")){
+            return "real2";
+        }
+
+        return profiles.get(0);
+    }
+
 }
